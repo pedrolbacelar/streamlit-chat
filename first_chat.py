bb2471ed8920903818cb7d4e6b203fc093be219f
Pedro Bacelar <pedrobacelarsantos@usp.br>
2024-04-25 15:25:10 +0100

    remove sheet

--- first_chat.py
+++ first_chat.py
@@ -2,17 +2,6 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
-#------------------------------------------
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read()
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Name} has a :{row.Pet}:")
-st.write("Hey :dog:")
-
-
 # ------------------------------------------
 st.title("Echo Bot")
 
